b81f936d4ac35cf11653c548dcee07c045d29ad4
sravya <yaragorllasraya@gmail.com>
2025-12-21 12:33:13 +0530

    Initial commit - Todo React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,51 @@
-import { Component } from "react";
+import {Component} from "react"
 import "./App.css";
+class App extends Component{
+    constructor(){
+        super();
+        this.state={todoInput:"", todos:[],isEdit:false,updateIndex:null}
+    }
+    handleInput= (e)=>{
+        this.setState({todoInput:e.target.value})
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todoInput: "",
-      todos: [],
-      isEdit: false,
-      updateIndex: null
-    };
-  }
+    }
+    handleAddTodo= ()=>{
+        const newtodo=[...this.state.todos,this.state.todoInput]
+          this.setState({todos:newtodo,todoInput:""})
+    }
+    handleDelete =(index)=>{
+      const todosdelete=this.state.todos.filter((ele,i)=>  i !==index)
+      this.setState({todos:todosdelete})
+    }
+    handleEdit = (index)=>{
+        const findTodoById=this.state.todos.find((ele,i)=> i ==index)
+        this.setState({todoInput:findTodoById,isEdit:true,updateIndex:index})
 
-  handleInput = (e) => {
-    this.setState({ todoInput: e.target.value });
-  };
-
-  handleAddTodo = () => {
-    if (!this.state.todoInput.trim()) return;
-
-    const newTodo = {
-      text: this.state.todoInput,
-      completed: false
-    };
-
-    this.setState({
-      todos: [...this.state.todos, newTodo],
-      todoInput: ""
-    });
-  };
-
-  handleDelete = (index) => {
-    const filteredTodos = this.state.todos.filter((_, i) => i !== index);
-    this.setState({ todos: filteredTodos });
-  };
-
-  handleEdit = (index) => {
-    this.setState({
-      todoInput: this.state.todos[index].text,
-      isEdit: true,
-      updateIndex: index
-    });
-  };
-
-  handleUpdate = () => {
-    const updatedTodos = [...this.state.todos];
-    updatedTodos[this.state.updateIndex].text = this.state.todoInput;
-
-    this.setState({
-      todos: updatedTodos,
-      todoInput: "",
-      isEdit: false,
-      updateIndex: null
-    });
-  };
-
-  toggleComplete = (index) => {
-    const updatedTodos = [...this.state.todos];
-    updatedTodos[index].completed = !updatedTodos[index].completed;
-    this.setState({ todos: updatedTodos });
-  };
-
-  render() {
+    }
+    handleUpdate = ()=>{
+        this.state.todos.splice(this.state.updateIndex,1,this.state.todoInput)
+        this.setState({todoInput:"",todos:this.state.todos,isEdit:false,updateIndex:null})
+    }
+ render(){
+   
     return (
-      <div className="main">
-        <h2>Todo Application</h2>
-
+    <div className="main">
+      <h1>Todo Application</h1>
         <div className="add">
-          <input
-            type="text"
-            placeholder="Enter todo task"
-            value={this.state.todoInput}
-            onChange={this.handleInput}
-          />
+        <input type="text" onChange={(e)=>this.handleInput(e)} placeholder="Enter todo Tasks" value={this.state.todoInput}/>  
+        {this.state.isEdit === true ? <button onClick={()=>this.handleUpdate()}>Update</button>: <button onClick={()=>this.handleAddTodo()}>Add</button>} 
+        
 
-          {this.state.isEdit ? (
-            <button onClick={this.handleUpdate}>Update</button>
-          ) : (
-            <button onClick={this.handleAddTodo}>Add</button>
-          )}
-        </div>
-
-        <div className="second">
-          {this.state.todos.map((todo, index) => (
-            <div
-              key={index}
-              className={todo.completed ? "todo completed" : "todo"}
-            >
-              <p onClick={() => this.toggleComplete(index)}>
-                {todo.text}
-              </p>
-
-              <div className="btns">
-                <button onClick={() => this.handleEdit(index)}>Edit</button>
-                <button onClick={() => this.handleDelete(index)}>Delete</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+    </div>
+    <div className="second">
+    {this.state.todos.map((ele,index)=> <div key={index}> 
+        <p>{ele}</p>
+        <button onClick={()=>this.handleEdit(index)}>Edit</button>
+        <button onClick={()=>this.handleDelete(index)}>Delete</button>
+    </div>)}
+    </div>
+    </div>
     );
-  }
+ }
 }
-
 export default App;
